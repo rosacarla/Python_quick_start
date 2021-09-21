bdfb751ba4546d09d22a338a2638611791be0aac
Carla Edila <rosa.carla@pucpr.edu.br>
2021-09-21 05:36:46 -0300

    parte 2

---
+++ variaveis.py
@@ -0,0 +1,18 @@
+#Codigo: Variaveis
+#Autora: Carla Edila Silveira
+#Finalidade: exemplo de parte do codigo de um jogo da forca - curso Python Quick Start
+#Data: 21/09/2021
+
+#começo do jogo da forca
+tentativas = 10 #variavel representa quantidade total de tentativas do jogo
+print(tentativas)
+
+#jogador 2 faz 1a tentativa
+tentativas -= 1 #variavel atualizada com subtracao de 1
+print(tentativas)
+
+#jogador tenta de novo
+tentativas -= 1   #variavel atualizada com subtracao de 1
+print(tentativas)
+
+#fim do codigo
